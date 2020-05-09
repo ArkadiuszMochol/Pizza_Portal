d080c7f893bd8ccb9835e4f39e6d56a1e902949f
Arkadiusz Mochol <mochol_arkadiusz@o2.pl>
2020-05-09 13:51:26 +0200

    Created method createWindow in WindowService class

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -3,13 +3,14 @@ package controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import jdk.jfr.Label;
 import service.LoginService;
 import service.WindowService;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.KeyEvent;
+import java.io.IOException;
 
 public class LoginController {
     LoginService loginService;
@@ -17,16 +18,14 @@ public class LoginController {
 
     @FXML
     private PasswordField pfPassword;
-
     @FXML
     private TextField tfLogin;
-
     @FXML
     private Label lblInfo;
 
     @FXML
     void keyLoginAction(KeyEvent keyEvent) {
-        if (keyEvent.getKeyCode() == KeyCode.ENTER) {
+        if (keyEvent.getCode() == KeyCode.ENTER) {
             loginService.login(tfLogin, pfPassword, lblInfo);
         }
     }
@@ -37,7 +36,8 @@ public class LoginController {
     }
 
     @FXML
-    void registerAction(ActionEvent event) {
+    void registerAction(ActionEvent event) throws IOException {
+        windowService.createWindow()
 
     }
 }
--- src/main/java/service/WindowService.java
+++ src/main/java/service/WindowService.java
@@ -1,4 +1,20 @@
 package service;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+import java.io.IOException;
+
 public class WindowService {
+    public void createWindow(String viewName, String title) throws IOException {
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/view/" + viewName + ".fxml"));
+        stage.setTitle(title);
+        stage.setScene(new Scene(root));
+        stage.setResizable(false);
+        stage.show();
+    }
 }
